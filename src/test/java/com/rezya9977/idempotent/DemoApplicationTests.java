75c7c4df6b9df37668dbd3f2400296ea4796c5bc
rezya9977 <ziranyang97@163.com>
2025-03-30 22:24:40 +0800

    Springboot初始化

---
+++ src/test/java/com/rezya9977/idempotent/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rezya9977.idempotent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
